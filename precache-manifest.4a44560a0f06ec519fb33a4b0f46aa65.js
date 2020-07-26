9b13cf72f50612c1d686e83c411c57bab4f83d5c
Gustavo Turra Perandré <gustavo1190.gt@gmail.com>
2020-07-25 22:28:03 -0300

    Updates

---
+++ precache-manifest.4a44560a0f06ec519fb33a4b0f46aa65.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c61a4103c1f498637b2d9d8d4aeca85",
+    "url": "/index.html"
+  },
+  {
+    "revision": "242c21f1969536cb5691",
+    "url": "/static/js/2.c5774dda.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.c5774dda.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f35676f536516f100aea",
+    "url": "/static/js/main.7362b4eb.chunk.js"
+  },
+  {
+    "revision": "23a8542c495de0a29ff2",
+    "url": "/static/js/runtime-main.f51e40a7.js"
+  },
+  {
+    "revision": "0b819d63338019a0f1cc7263e9c166ae",
+    "url": "/static/media/github-background.0b819d63.svg"
+  },
+  {
+    "revision": "04f6798f67fd4e48d053d05d18d9bd83",
+    "url": "/static/media/logo.04f6798f.svg"
+  }
+]);
